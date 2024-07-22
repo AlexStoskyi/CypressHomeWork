4b22a74b503d94bff6c01b67f927f88cd30aff49
AlexStoskyi <alexstoskyi@gmail.com>
2024-07-22 19:46:29 +0200

    add gitignor / id / token

--- cypress/e2e/1 invalid_Registration.cy.ts
+++ cypress/e2e/1 invalid_Registration.cy.ts
@@ -4,7 +4,7 @@ import { singUpPage } from "../pages/singUp.page";
 
 describe('Registration', () => {
   it('#1 Registration with invalid data', () => {
-    const fullName = faker.name.firstName();
+    const fullName = faker.internet.userName;
     const randomEmail = faker.internet.email();
     const randomPassword = faker.internet.password();
 
@@ -13,9 +13,9 @@ describe('Registration', () => {
 
     mainPage.visitLoginPage();
     mainPage.elements.singUpButton().click();
-    singUpPage.SingUp(fullName, invalidEmail1, randomPassword);
+    singUpPage.SingUp(fullName(), invalidEmail1, randomPassword);
     mainPage.elements.errorMsg().should('be.visible');
 
-  //Буде помилка томущо регестрація без @ проходить
+    //Буде помилка томущо регестрація без @ проходить
   });
 });
